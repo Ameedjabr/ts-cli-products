4b7b09d91ae8f58085b8a1b702d8eb3f862ea7ff
Ameedjabr <git config --global user.email>
2026-03-24 11:33:05 +0200

    updated package.json and config

--- src/config/db.ts
+++ src/config/db.ts
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER!,
   process.env.DB_PASS!,
   {
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST!,
     port: Number(process.env.DB_PORT),
     dialect: "postgres",
     logging: false,
